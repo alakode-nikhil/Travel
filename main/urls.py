d9abdbc5026e8cc634741df78200ad09d78101ec
alakode-nikhil <alakodenikhil@gmail.com>
2024-10-30 14:52:56 +0530

    add index views and index template

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('api/list-destination/<int:pk>/', ListDestinationApi.as_view(), name='api-list-destination'),
     path('api/update-destination/<int:pk>/', UpdateDestinationApi.as_view(), name='api-update-destination'),
     path('api/delete-destination/<int:pk>/', DeleteDestinationApi.as_view(), name='api-delete-destination'),
-    path('api/search-destination/<str:name>/', SearchDestinationApi.as_view(), name='api-search_destination'),  
+    path('api/search-destination/<str:name>/', SearchDestinationApi.as_view(), name='api-search_destination'),
+    path('', index,name='home-destination'),  
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,9 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib import messages
+import requests
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
 
@@ -42,3 +45,57 @@ class SearchDestinationApi(generics.ListAPIView):
             return Response({"message": "No destinations found."}, status=status.HTTP_404_NOT_FOUND)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+    
+def index(request):
+
+    if request.method =='POST':
+        search = request.POST['search']
+
+        api_url = f'http:127.0.0.1:8000/api/search-destination/{search}/'
+
+        try:
+            response = requests.get(api_url)
+            print(response.status_code)
+
+            if response.status_code == 200:
+                data = response.json()
+
+            else:
+                data = None
+
+        except requests.RequestException as e:
+            data = None
+
+        return render(request, 'index.html', {'data':data})
+    
+    else:
+
+        api_url = 'http://127.0.0.1:8000/api/create-destination/'
+
+        try:
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                data = response.json()
+                original_data = data
+
+                paginator = Paginator(original_data, 10)
+                page = request.GET.get('page', 1)
+
+                try:
+                    destinations = paginator.page(page)
+                except (EmptyPage, InvalidPage):
+                    destinations = paginator.page(paginator.num_pages)
+
+                context = {
+                    'original_data': original_data,
+                    'destinations': destinations
+                }
+
+                return render(request, 'index.html', context)
+            
+            else:
+
+                return render(request, 'index.html', {'error_message':f'Error {response.status_code}' })
+        
+        except requests.RequestException as e:
+            return render(request, 'index.html', {'error_message':f'Error {e}'})
